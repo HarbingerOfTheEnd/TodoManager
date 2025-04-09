0373a8b8fed1e75154db7a260bef50324b425ebd
Sai Kumar <ksai44576@gmail.com>
2025-04-09 21:18:43 +0530

    feat: add EmptyState component for task management interface

---
+++ components/EmptyState.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Message = styled.Text`
+    font-size: 18px;
+    color: ${(props) => props.theme.colors.text};
+`;
+
+export default function EmptyState(): JSX.Element {
+    return (
+        <Container>
+            <Message>No tasks yet! Add one to get started.</Message>
+        </Container>
+    );
+}
