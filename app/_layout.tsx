11e64df302e26f0c3aa517fd51e426c239189184
Sai Kumar <ksai44576@gmail.com>
2025-04-09 21:23:08 +0530

    feat: refactor RootLayout to use ThemeProvider and remove unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,11 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import theme from '@/styles/theme';
+import { Stack } from 'expo-router';
+import { ThemeProvider } from 'styled-components/native';
 
-export { ErrorBoundary } from 'expo-router';
-
-export const unstable_settings = {
-    initialRouteName: '(auth)',
-};
-
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-    const [loaded, error] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-        ...FontAwesome.font,
-    });
-
-    useEffect(() => {
-        if (error) throw error;
-    }, [error]);
-
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
-    }
-
-    return <></>;
+export default function RootLayout(): JSX.Element {
+    return (
+        <ThemeProvider theme={theme}>
+            <Stack screenOptions={{ headerShown: false }} />
+        </ThemeProvider>
+    );
 }
