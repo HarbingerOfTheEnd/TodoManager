785bd29bed4ebb142ecc44af41db6c5b16ac8b01
Sai Kumar <ksai44576@gmail.com>
2025-04-09 22:12:04 +0530

    refactor: wrap RootLayout in GestureHandlerRootView for improved gesture handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import theme from '@/styles/theme';
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ThemeProvider } from 'styled-components/native';
 
 export default function RootLayout(): JSX.Element {
     return (
-        <ThemeProvider theme={theme}>
-            <Stack screenOptions={{ headerShown: false }} />
-        </ThemeProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <ThemeProvider theme={theme}>
+                <Stack screenOptions={{ headerShown: false }} />
+            </ThemeProvider>
+        </GestureHandlerRootView>
     );
 }
